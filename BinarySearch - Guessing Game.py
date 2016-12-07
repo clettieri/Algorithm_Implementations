2716ad7ff3303d4713b6c351613717b0cde23670
clettieri <lettieri.christopher@gmail.com>
2016-12-07 13:20:32 -0500

    start game function

--- BinarySearch - Guessing Game.py
+++ BinarySearch - Guessing Game.py
@@ -22,7 +22,25 @@ def start_game():
     range high from the user.  It will then state how many guesses
     the computer will need and the game will begin.
     '''
-    pass
+    print "Welcome to the Binary Search - Pick a Number Guessing Game!"
+    print "All you have to do is pick a range from 1 to Any Positive Number"
+    print "And then pick a number in the range.  After each guess I make, "
+    print "please tell me if your guess is higher, lower, or if I guessed right!"
+    print ""
+    while True:
+        try:
+            print "What is the maximum range you want to play in, from 1 to ?"
+            max_range = int(raw_input(">"))
+        except ValueError:
+            print "That was not a valid number, please try again."
+            continue
+        else:
+            guesses_needed = compute_guesses(max_range)
+            print "Very well, I will need " + str(guesses_needed) + " guesses."
+            break
+    
+        
+    
 
 def compute_guesses(n):
     '''(integer) -> integer
@@ -34,3 +52,4 @@ def compute_guesses(n):
     guesses = math.log(n, 2)
     return int(math.ceil(guesses))
     
+start_game()
