076318f1641fac950485e430061efcfda0115e5e
clettieri <lettieri.christopher@gmail.com>
2016-12-07 12:53:30 -0500

    Initial Commit

---
+++ BinarySearch - Guessing Game.py
@@ -0,0 +1,24 @@
+"""
+Binary Search - Pick a Number Guessing Game!
+
+In this game you will define a range from 1 to n.
+Then you must pick a number in that range.
+The computer will tell you how many guess at most it will
+need to guess your number.
+
+Each guess the computer makes the user will say if their number
+is higher, lower, or exactly right.
+
+This game is a demonstration of binary search.
+Binary search find's an element in a sorted list by continually halving
+the search space until the element is found.
+"""
+
+def start_game():
+    '''(None) -> integer
+    
+    This function will print the instructions to the screen, and get the
+    range high from the user.  It will then state how many guesses
+    the computer will need and the game will begin.
+    '''
+    pass
