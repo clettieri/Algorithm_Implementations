16c2f6c96928996686e3a611c2cf961c54cec699
clettieri <lettieri.christopher@gmail.com>
2016-12-10 08:57:09 -0500

    Find min index working

--- SelectionSort.py
+++ SelectionSort.py
@@ -9,3 +9,39 @@ smallest value and place it at index 1, This looping over a
 subarray will continue until all items are sorted.
 """
 
+def swap_values(l, first_index, second_index):
+    '''(list, int, int) -> None
+    
+    Will take the first_index and swap it with the second_index.
+    This function modifies the original list.
+    '''
+    pass
+
+def find_minimum_index(l, start_index):
+    '''(list, int) -> int
+    
+    Will loop through the list starting at start_index
+    and return the index of the minimum value.
+    '''
+    min_value = float('inf')
+    min_index = None
+    for i in range(start_index, len(l)):
+        if l[i] < min_value:
+            min_value = l[i]
+            min_index = i
+    return min_index
+
+def selection_sort(l):
+    '''(list) -> list that is sorted
+    
+    Takes a list, will use selection sort to return
+    a sorted list.
+    '''
+    start_index = 0
+
+test_list = [17, 8, 1, 35, 0, 4, 6, 21, 22, 3, 0]
+print selection_sort(test_list)
+
+a = [0, 0, 5, 1, 6, 7]
+assert find_minimum_index(a, 2) == 3
+assert find_minimum_index(a, 1) == 1
