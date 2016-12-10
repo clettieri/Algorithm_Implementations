55bf96ceedbc7451b844709d21fa52aea977e439
clettieri <lettieri.christopher@gmail.com>
2016-12-10 08:38:46 -0500

    Selection Sort added

---
+++ SelectionSort.py
@@ -0,0 +1,11 @@
+"""
+Selection Sort
+
+Selection sort takes an array and sorts it from smallest
+to biggest by looping through each value of the array and
+finding the smallest value to place at index 0.  It then
+loops through a subarray starting at index 1 to find the next
+smallest value and place it at index 1, This looping over a 
+subarray will continue until all items are sorted.
+"""
+
