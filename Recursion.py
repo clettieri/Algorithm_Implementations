fc2559532a51a3396bec0b34fcae3a08630d0e61
clettieri <lettieri.christopher@gmail.com>
2016-12-13 13:41:47 -0500

    is_palindrome() completed

--- Recursion.py
+++ Recursion.py
@@ -5,6 +5,7 @@ Base Case - this is what causes the recursion to stop and actually return a valu
 Recursive Case - this is when we call the function on itself.
 """
 
+"""FACTORIAL"""
 def factorial(n):
     '''(int) -> int
     
@@ -22,4 +23,32 @@ def factorial(n):
 assert factorial(0) == 1
 assert factorial(4) == 24
 assert factorial(5) == 120
-print "Success"
+print "Success - Factorial"
+
+
+"""PALINDROME"""
+def is_palindrome(s):
+    '''(string) -> bool
+    
+    Will test if word is a palindrome by recurisvely checking the first and last
+    letters to be the same.  Base case is a string of one or zero characters.
+    '''
+    #Format string
+    s = s.upper()    
+    
+    #Base Case
+    if len(s) <= 1:
+        return True
+    
+    #Recursive Case
+    if s[0] == s[-1]:
+        #First & Last match, test substring
+        return is_palindrome(s[1:-1])
+    else:
+        return False
+        
+assert is_palindrome("") == True
+assert is_palindrome("cat") == False
+assert is_palindrome("Rotor") == True
+assert is_palindrome("racecar") == True
+print "Success - Palindrome"
